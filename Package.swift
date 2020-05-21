ef9d0d1bc97d72b7b00d80d9c2af5e6fba8388cd
Jim Roepcke <jim@roepcke.com>
2020-05-20 23:21:00 -0700

    Make RCSSwiftUI depend on RCSFoundation and ComposableArchitecture

--- Package.swift
+++ Package.swift
@@ -17,13 +17,19 @@ let package = Package(
   dependencies: [
     // Dependencies declare other packages that this package depends on.
     // .package(url: /* package url */, from: "1.0.0"),
+    .package(name: "RCSFoundation", url: "https://github.com/JimRoepcke/swift-rcs-foundation.git", .branch("master")),
+    .package(name: "swift-composable-architecture", url: "https://github.com/JimRoepcke/swift-composable-architecture.git", .branch("master"))
   ],
   targets: [
     // Targets are the basic building blocks of a package. A target can define a module or a test suite.
     // Targets can depend on other targets in this package, and on products in packages which this package depends on.
     .target(
       name: "RCSSwiftUI",
-      dependencies: []),
+      dependencies: [
+        "RCSFoundation",
+        .product(name: "ComposableArchitecture", package: "swift-composable-architecture")
+      ]
+    ),
     .testTarget(
       name: "RCSSwiftUITests",
       dependencies: ["RCSSwiftUI"]),
